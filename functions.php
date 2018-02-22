2d0340051f0b8977182176e030928b6022589d11
josazar <josazar@gmail.com>
2018-02-22 10:37:59 +0100

    Delete Emoji script

--- functions.php
+++ functions.php
@@ -85,3 +85,11 @@ function neutral_wp_title( $title, $sep )
 	return $title;
 }
 add_filter( 'wp_title', 'neutral_wp_title', 10, 2 );
+
+/**
+ * Remove EMOJI Script
+ */
+ remove_action('wp_head', 'print_emoji_detection_script', 7);
+ remove_action('admin_print_scripts', 'print_emoji_detection_script');
+ remove_action('wp_print_styles', 'print_emoji_styles');
+ remove_action('admin_print_styles', 'print_emoji_styles');
