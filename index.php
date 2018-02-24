e055c1875ba4eafed68846ad84ce48654b0a73de
josazar <josazar@gmail.com>
2018-02-24 02:28:08 +0100

    reset app.js

--- index.php
+++ index.php
@@ -1,20 +1,40 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage neutral
- */
-?>
-
-<?php get_header(); ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-		<?php get_template_part( 'content', get_post_format() ); ?>
-	<?php endwhile; ?>
-<?php get_footer(); ?>
+<?php
+
+/**
+
+ * The main template file.
+
+ *
+
+ * This is the most generic template file in a WordPress theme
+
+ * and one of the two required files for a theme (the other being style.css).
+
+ * It is used to display a page when nothing more specific matches a query.
+
+ * E.g., it puts together the home page when no home.php file exists.
+
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+
+ *
+
+ * @package WordPress
+
+ * @subpackage neutral
+
+ */
+
+?>
+
+
+
+<?php get_header(); ?>
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'content', get_post_format() ); ?>
+
+	<?php endwhile; ?>
+
+<?php get_footer(); ?>
+
